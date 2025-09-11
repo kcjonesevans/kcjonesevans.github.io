6e1ca1c62e7a5003ba35d3355cb96c9c587fcf2a
KC Jones-Evans <kcjonesevans.gmail.com>
2025-09-11 10:54:08 -0400

    add navigation

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,6 +1,8 @@
 // filepath: src/components/MainLayout/MainLayout.tsx
 import React from 'react';
+
 import Header from '../Header/Header';
+import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 import styles from './MainLayout.module.scss';
 
@@ -12,6 +14,7 @@ export default function MainLayout({ children }: Props) {
   return (
     <div className={styles.layout}>
       <Header />
+      <Nav />
       <main className={styles.main}>{children}</main>
       <Footer />
     </div>
